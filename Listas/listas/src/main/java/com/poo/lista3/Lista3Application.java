6987dcacc3ab5f77825528f1354098378bee73b4
rafaelfreire3107 <rafaelfreire3107@gmail.com>
2024-09-04 15:54:31 -0300

    Update Lista3Application.java

--- Listas/listas/src/main/java/com/poo/lista3/Lista3Application.java
+++ Listas/listas/src/main/java/com/poo/lista3/Lista3Application.java
@@ -11,8 +11,9 @@ public class Lista3Application {
     public static void main(String[] args) throws InterruptedException {
 		SpringApplication.run(Lista3Application.class, args);
 
-        //Exercicio1.resolucao(); // Acertar a resolução 
-        Exercicio2.resolucao();
+        //Exercicio1.resolucao(); 
+        //Exercicio2.resolucao();
+ 	//Exercicio3.resolucao();
 
 
 

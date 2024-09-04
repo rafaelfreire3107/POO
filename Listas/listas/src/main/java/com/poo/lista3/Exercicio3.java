755bd699d6aeb2aa9e73841ccad22e780411add7
rafaelfreire3107 <rafaelfreire3107@gmail.com>
2024-09-04 15:53:40 -0300

    Update Exercicio3.java

--- Listas/listas/src/main/java/com/poo/lista3/Exercicio3.java
+++ Listas/listas/src/main/java/com/poo/lista3/Exercicio3.java
@@ -1,5 +1,28 @@
 package com.poo.lista3;
-
+import java.util.Scanner;
+import org.springframework.boot.system.SystemProperties;
+/*3) Escreva um código que receba dois números inteiros e escreva todos os
+números pares entre eles. */
 public class Exercicio3 {
+public static void resolucao() {
+        Scanner scanner = new Scanner(System.in);
 
+                System.out.println("Digite o primeiro número:");
+                int num1 = scanner.nextInt();
+                System.out.println("Digite o segundo número:");
+                int num2 = scanner.nextInt();
+
+                int inicio = Math.min(num1, num2);
+                int fim = Math.max(num1, num2);
+
+                System.out.println("Números pares entre " + inicio + " e " + fim + ":");
+
+                for (int i = inicio; i <= fim; i++) {
+                    if (i % 2 == 0) {
+                        System.out.println(i);
+                    }
+                }
+
+                scanner.close();
+            }
 }

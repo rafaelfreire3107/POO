5ad6a809c125a0e4d4134aed69ea9bb978a73bdc
rafaelfreire3107 <rafaelfreire3107@gmail.com>
2024-09-05 15:06:45 -0300

    Update Exercicio4.java

--- Listas/listas/src/main/java/com/poo/lista3/Exercicio4.java
+++ Listas/listas/src/main/java/com/poo/lista3/Exercicio4.java
@@ -1,6 +1,9 @@
 package com.poo.lista3;
 
 import java.util.Scanner;
+/* 4) Jogo da adivinhação. Peça para alguém escolher um número. Depois peça
+        para outra pessoa tentar adivinhar. O programa deverá retornar dicas como:
+        "Muito baixo, muito alto ou quase lá" ou "tá quente, tá frio". */
 
 public class Exercicio4 {
     public static void resolucao(){

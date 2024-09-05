753f05753f6bea973dfbd8995c0727b86992ea5a
rafaelfreire3107 <rafaelfreire3107@gmail.com>
2024-09-05 17:32:57 -0300

    Add files via upload

---
+++ Listas/listas/src/main/java/com/poo/lista4/Exercicio4.java
@@ -0,0 +1,15 @@
+package com.poo.lista4;
+
+public class Exercicio4 {
+public static void resolucao(){
+    
+    int lista[] = {3, 5, 6, 7, 8, 10, 22, 55, 110};
+
+    for (int i = 0; i < 9 ; i++){
+        if (lista[i] % 2 == 0){
+            System.out.println(lista[i]);
+        }
+    }
+}
+
+}

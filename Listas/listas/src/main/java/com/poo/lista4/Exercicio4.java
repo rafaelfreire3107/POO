225e75bf7a73c951080ce7e56141e2da1ca77a78
rafaelfreire3107 <rafaelfreire3107@gmail.com>
2024-09-05 17:42:25 -0300

    Update Exercicio4.java

--- Listas/listas/src/main/java/com/poo/lista4/Exercicio4.java
+++ Listas/listas/src/main/java/com/poo/lista4/Exercicio4.java
@@ -4,12 +4,14 @@ public class Exercicio4 {
 public static void resolucao(){
     
     int lista[] = {3, 5, 6, 7, 8, 10, 22, 55, 110};
-
+    int contaPares = 0;
     for (int i = 0; i < 9 ; i++){
         if (lista[i] % 2 == 0){
-            System.out.println(lista[i]);
+            lista[i] += contaPares;
+            contaPares++;
         }
     }
+        System.out.println("Números pares no vetor = " +contaPares);
 }
 
 }

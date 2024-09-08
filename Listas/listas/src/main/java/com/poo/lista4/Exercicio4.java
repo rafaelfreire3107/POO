a1ef61750dd8461441c343bd0711a23670727fda
rafaelfreire3107 <rafaelfreire3107@gmail.com>
2024-09-07 22:49:45 -0300

    Update Exercicio4.java

--- Listas/listas/src/main/java/com/poo/lista4/Exercicio4.java
+++ Listas/listas/src/main/java/com/poo/lista4/Exercicio4.java
@@ -1,4 +1,7 @@
 package com.poo.lista4;
+/*4) Escreva um código que irá percorrer uma lista de números inteiros
+lista[ ] = { 3, 5, 6, 7, 8, 10, 22, 55, 110 } e irá contar a quantidade de
+números pares presente nela.*/
 
 public class Exercicio4 {
 public static void resolucao(){

45411c6b026db519302527a90fce7da875e7b0e3
rafaelfreire3107 <rafaelfreire3107@gmail.com>
2024-09-07 22:48:17 -0300

    Update Exercicio1.java

--- Listas/listas/src/main/java/com/poo/lista4/Exercicio1.java
+++ Listas/listas/src/main/java/com/poo/lista4/Exercicio1.java
@@ -1,7 +1,9 @@
 package com.poo.lista4;
 
 import java.util.Scanner;
-
+/*1) Voce é gerente de um supermercado e sabe que os valores das
+vendas do dia são gravados em um vetor. Digite um código que faça o
+fechamento (soma) de vendas do dia de maneira automatizada.*/
 public class Exercicio1 {
     public static void resolucao(){
 
